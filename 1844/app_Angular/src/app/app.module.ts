e1ab7d6d2adeae81d8b7f794a789950b228680f3
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-08-25 15:40:33 +0100

    Estilimos del header y otras pequeñas modificaciones

--- 1844/app_Angular/src/app/app.module.ts
+++ 1844/app_Angular/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+
+//Nuestros componentes
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { CursoComponent } from './curso/curso.component';
