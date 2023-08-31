fd879cdc02038b96c1356c2e6f2046568acc30e0
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-08-31 14:29:27 +0100

    cambios de ayer

--- 1844/app_Angular/src/app/consulta/consulta.component.ts
+++ 1844/app_Angular/src/app/consulta/consulta.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.component.html',
   styleUrls: ['./consulta.component.css']
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit{
+  
 
+  contacto!:FormGroup;
+  // submitted: false;
+  title="Formulario de contacto"
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
