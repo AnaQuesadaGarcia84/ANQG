e782708273e909e4ca2272ff2caa9be8651988cb
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-09-06 16:37:55 +0100

    editando las rutas

--- 1844/app_Angular/server_node/routes/user.js
+++ 1844/app_Angular/server_node/routes/user.js
@@ -1,15 +1,17 @@
 const express = require('express')
 
-const controller =require('../controllers/user')
+const UserController =require('../controllers/user')
 const router= express.Router()
 
 const path='user'
 
-router.get(
-    `/${path}`,
-    controller.getData
-    )
-
+router.get('/home', UserController.home);
+router.get('/test', UserController.test);
+router.get('/save-user', UserController.saveUser);
+router.get('/user/:id?', UserController.getUser);
+router.get('/users', UserController.getUsers);
+router.get('/user/:id', UserController.updateUser);
+router.get('/user/:id', UserController.deleteUser);
 
 module.exports = router
 
