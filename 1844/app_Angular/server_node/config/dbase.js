841e6b0a035fdaf794803da1a91af2e6677f52cb
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-09-06 15:34:24 +0100

    inicio de clase

--- 1844/app_Angular/server_node/config/dbase.js
+++ 1844/app_Angular/server_node/config/dbase.js
@@ -1,19 +1,19 @@
 const mongoose= require('mongoose')
 
-const DB_URI='mongodb://localhost:27017/Appweb'
+// const DB_URI='mongodb://localhost:27017/Appweb'
 
-module.exports=()=>{
-    const connect= ()=>{
-        mongoose.connect(DB_URI,
-            {
-                keepAlive:true,
-                useNewUrlParse:true,
-                useUnifieldTopology: true
-            }
-        ).then(()=>{
-            console.log('Conexión correcta')})
-        .catch((err)=>{'DB:ERROR !!'})
-    }
+// module.exports=()=>{
+//     const connect= ()=>{
+//         mongoose.connect(DB_URI,
+//             {
+//                 keepAlive:true,
+//                 useNewUrlParse:true,
+//                 useUnifieldTopology: true
+//             }
+//         ).then(()=>{
+//             console.log('Conexión correcta')})
+//         .catch((err)=>{'DB:ERROR !!'})
+//     }
 
-    connect()
-}
+//     connect()
+// }
--- 1844/app_Angular/server_node/server.js
+++ 1844/app_Angular/server_node/server.js
@@ -1,22 +1,32 @@
 const express=require('express')
 
-const initDB = require('./config/dbase')
-
-const userRouters= require('./routes/user')
+//const userRouters= require('./app/routes/user')
 
 const app = express()
 
-const port = 3001
+const port = 3700
 
-// app.get('/',(req, res)=>{
-//     res.send({
-//         data:'Bienvenido Mister Marshall'
-//     })
-// })
-
-app.use(userRouters)
-app.link(port, ()=>{
-    console.log('En línea!!!')
+app.listen(port,()=>{
+    console.log('La aplicación está en línea...')
 })
 
-initDB()
+//app.use(userRouters)
+
+//Conexión con la base de datos
+const mongoose=require('mongoose')
+
+mongoose.Promise= global.Promise
+mongoose.connect('mongodb://127.0.0.1:27017/AppWeb', {
+    useNewUrlParser:true,
+})
+.then(()=>{
+    console.log('Conexión establecida...')
+})
+.catch(err => console.log(err))
+
+
+
+
+
+
+//initDB()
