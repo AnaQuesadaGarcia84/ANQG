31f5a60cf962a5acd22fab41e874238fff3756b6
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-09-06 15:48:44 +0100

    cambios

--- 1844/app_Angular/server_node/controllers/user.js
+++ 1844/app_Angular/server_node/controllers/user.js
@@ -1,11 +1,37 @@
-//res.send({data: 'Esto viene de el controlador usuario'})
 const model= require('../models/user');
+const fs=require('fs');
+const { Model } = require('mongoose');
+const path=require('path')
 
-exports.getData = (req, res) =>{
-    model.find({}, (err,docs)=>{
-        res.send({
-            doc:docs
-        })
-    })
+const controller ={
+    home: function(req, res){
+        return res.status(200).send({
+            message: 'Soy el home'
+        });
+    },
+    test: function(req,res){
+        return res.status(200).send({
+            message: 'Soy el método o acción test del controlador user'
+        });
+    },
+    saveUser: function(req, res){
+        const user= new Model();
+        const params = req.body;
+        user.name= params.name;
+        user.description = params.description;
+        user.category = params.category;
+        user.year= params.year;
+        user.langs = params.langs;
+
+
+
+
+
+
+
+
+
+
+        
+    }
 }
-
