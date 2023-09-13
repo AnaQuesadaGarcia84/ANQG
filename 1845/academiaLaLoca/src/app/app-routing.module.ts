5eca5fbd2534215dac420480e00838259b0819c1
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-09-13 19:42:04 +0100

    Crear componentes en el proyecto

--- 1845/academiaLaLoca/src/app/app-routing.module.ts
+++ 1845/academiaLaLoca/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Mis componentes
+import { ContactoComponent } from './contacto/contacto.component';
+import { RegistroComponent } from './registro/registro.component';
+
+
+const routes: Routes = [
+  {path: 'contacto', component: ContactoComponent},
+  {path: 'registro', component: RegistroComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 1845/academiaLaLoca/src/app/app.module.ts
+++ 1845/academiaLaLoca/src/app/app.module.ts
@@ -5,14 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Mis componentes
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+//Angular
+import {MatInputModule} from '@angular/material/input';
+import { RegistroComponent } from './registro/registro.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactoComponent,
+    RegistroComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
