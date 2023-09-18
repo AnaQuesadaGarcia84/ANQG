77c8b0b33c912140cf6e58fb7e23b6b16285ef8e
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-09-18 18:08:40 +0100

    cambios

--- evaluable_18_09/src/app/tareas/tareas.component.ts
+++ evaluable_18_09/src/app/tareas/tareas.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import { ToastrService } from 'ngx-toastr'
+
+//Servicio - Modelo
+import { Tareas } from '../models/tareas';
+import { TareasService } from '../service/tareas';
 
 @Component({
   selector: 'app-tareas',
@@ -6,10 +13,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tareas.component.css']
 })
 export class TareasComponent implements OnInit {
+  tareasForm: FormGroup;
+  id: string | null
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService,
+    private _tareasService: TareasService,
+    private aRouter: ActivatedRoute) {
 
-  ngOnInit() {
+    this.tareasForm = this.fb.group({
+      user: ['', Validators.required],
+      idTarea: ['', Validators.required],
+      descriptionTarea: ['', Validators.required],
+      puntuacion: ['', Validators.required],
+      estado: ['', Validators.required]
+    })
+    this.id = this.aRouter.snapshot.paramMap.get('id');
+  }//Fin del constructor
+
+  ngOnInit(): void {
+    
   }
 
 }
