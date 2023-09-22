68f503101258092e6bc9b2f4b070a6c4684881da
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-09-22 16:08:43 +0100

    cambios evaluable

--- evaluable_18_09/server_node/server.js
+++ evaluable_18_09/server_node/server.js
@@ -6,16 +6,16 @@ const cors = require("cors")
 // Creamos el servidor
 const app = express()
 
+// Conectamos DB appweb
 conectarDB();
 app.use(cors())
 
 app.use(express.json());
 
 app.use('/api/cursos', require('./app/routes/cursoroutes'));
-app.use('/api/tareas', require('./app/routes'))
+app.use('/api/usuarios', require('./app/routes/usuarioroutes'));
+app.use('/api/mensajes', require('./app/routes/mensajeroutes'));
 
 app.listen(3700, () => {
     console.log('El servidor esta corriendo perfectamente')
 })
-
-
