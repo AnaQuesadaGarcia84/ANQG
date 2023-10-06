f2b1e445059c05641a6ec2cbb6c305496fb521e7
AnaQuesada84 <ananadiaquesadagarcia@gmail.com>
2023-10-06 19:45:22 +0100

    cambios en mi proyecto

--- blog/src/app/app-routing.module.ts
+++ blog/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Importamos los componentes creados que vamos a usar en la aplicación
+import { HomeComponent } from './componentes/home/home.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'contacto', component: ContactoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HeaderComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactoComponent,
    ],
   imports: [
     BrowserModule,
